9c248db765d4c2d728765c24fb3fd97fc7329648
yougain <yougain@LAVIE2.localdomain>
2020-01-24 17:20:36 +0900

    Fist commit

---
+++ tz_stl_auto/src/stl_set_tz_ruby.c
@@ -0,0 +1,107 @@
+﻿#include "set_tz"
+#include <algorithm>
+#include <ruby.h>
+#include <new>
+
+
+using namespace std;
+using namespace tz;
+
+
+void Init_ext_set(void){
+	VALUE cESet = rb_define_class("ESet", rb_cObject);
+	rb_define_method(cESet, "initialize", eSet_initialize, -1);
+	rb_define_method(cESet, "begin", eSet_begin, -1);
+	rb_define_method(cESet, "end", eSet_end, -1);
+	rb_define_method(cESet, "insert", eSet_insert, -1);
+
+	VALUE cESetIt = rb_define_class("ESetIt", rb_cObject);
+	rb_define_method(cESetIt, "inc", eSetIt_inc, -1);
+	rb_define_method(cESetIt, "dec", eSetIt_dec, -1);
+	rb_define_method(cESetIt, "it", eSetIt_it, -1);
+
+	rb_define_method(cKernel, "find", cKernel_find, -1);
+	rb_define_method(cKernel, "erase", cKernel_erase, -1);
+	rb_define_method(cKernel, "for_each", cKernel_for_each, -1);
+}
+
+struct LessESet{
+	bool operator<(VALUE a, VALUE b) const;
+}
+
+class ESet{
+	set* _s;
+	typedef bool (*CompareBy)(VALUE);
+	typedef bool (*Less)(VALUE, VALUE);
+public:
+	ESet() : _s(new set<VALUE>){ }
+	ESet(CompareBy f) : _s(new set_by<VALUE>(f)){ }
+	ESet(Less f) : _s(new set_less<VALUE>(f)){ }
+	~ESet(){ delete _s; }
+	bool isLeagal();
+}
+
+struct ValueCompareBy{
+	VALUE block;
+	typedef VALUE InitArgType;
+	ValueCompareBy(InitArgType b) : block(b){ }
+	bool operator()(VALUE a, VALUE b){
+		return *** block.method.call(a) < block.method.call(b);
+	}
+};
+
+
+struct ValueLess{
+	VALUE block;
+	typedef VALUE InitArgType;
+	ValueLess(InitArgType b) : block(b){ }
+	bool operator()(VALUE a, VALUE b){
+		return *** block.method.call(a, b);
+	}
+};
+
+
+typedef setbase<VALUE> ESet;
+typedef set<VALUE, CompareBy> ESetCompareBy;
+typedef set<VALUE, CompareLess> ESetCompareLess;
+
+
+static void wrap_ESet_free(ESet* p){
+	if(p->isLeagal()) p->~ESet();
+	ruby_xfree(p);
+}
+
+
+static void wrap_ESet_mark(ESet* p){
+	for_each(p->begin(), p->end(), 
+		[](VALUE v){
+			rb_gc_mark(v);
+		}
+	);
+}
+
+// ESet.new do |a, b|
+//   a < b
+// end
+
+static VALUE wrap_ESet_alloc(VALUE klass){
+	return Data_Wrap_Struct(klass, wrap_ESet_mark, wrap_ESet_free, ruby_xmalloc(sizeof(ESetCompareLess)));
+}
+
+
+static VALUE wrap_ESet_init(VALUE self, VALUE block) {
+	ESet* p;
+	if(block is no arg){
+		Data_Get_Struct(self, ESet, p);
+		new(p) ESet();
+	}else if(block has one arg){
+		Data_Get_Struct(self, ESetBy, p);
+		new(p) ESetCompareBy(block);
+	}else{
+		Data_Get_Struct(self, ESetLess, p);
+		new(p) ESetCompareLess(block);
+	}
+}
+
+
+
---
+++ tz_stl_auto/src/tz_stl_auto.cpp
@@ -0,0 +1,84 @@
+﻿//============================================================================
+// Name        : tz_stl_auto.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "set_tz"
+#include <stdlib.h>
+#include <algorithm>
+#include <typeinfo>
+using namespace std;
+using namespace tz;
+
+#include <cxxabi.h>
+
+
+char* demangle(const char *demangled)
+{
+	int status;
+	return abi::__cxa_demangle(demangled, 0, 0, &status);
+}
+
+void main2();
+
+int main() {
+	main2();
+	return 0;
+}
+
+void main2(){
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	tz::multiset<int>& s(*new tz::multiset<int>);
+	std::cout << __LINE__ << std::endl;
+	std::cout << __LINE__ << std::endl;
+	for(int i = 0; i < 10 ; i++){
+		std::cout << __LINE__ << std::endl;
+		s.insert(rand());
+		std::cout << __LINE__ << std::endl;
+	}
+	std::cout << __LINE__ << std::endl;
+	/*std::for_each(s.begin(), s.end(), [](int x) {
+		std::cout << x << std::endl;
+	});*/
+	std::cout << "it_test" << std::endl;
+	multiset<int>::iterator it = s.begin(), it2, it3, it4, it5;
+	++it;
+	it5 = it4 = it2 = it3 = it;
+	for(multiset<int>::iterator it = s.begin(); it != s.end() ; ++it){
+		std::cout << *it << std::endl;
+		std::cout << *(void**)&it << ";" << it._M_node->_TZ_refCount << std::endl;
+	}
+	std::cout << __LINE__ << std::endl;
+	std::cout << *s.end() << std::endl;
+	std::cout << __LINE__ << std::endl;
+	std::cout << demangle(typeid(s.end()).name()) << std::endl;
+	std::cout << "adr:" << &s << std::endl;
+	std::cout << "ptr_test" << std::endl;
+	std::cout << "1:" << *(void**)&it << ":" << *it << std::endl;
+	++it;
+	std::cout << "2:" << *(void**)&it << ":" << *it << std::endl;
+	++it;
+	std::cout << "3:" << *(void**)&it << ":" << *it << std::endl;
+	++it;
+	std::cout << "4:" << *(void**)&it << ":" << *it << std::endl;
+	++it;
+	std::cout << "5:" << *(void**)&it << ":" << *it << std::endl;
+	std::cout << "ptr_test2" << std::endl;
+	it = s.begin();
+	it2 = it3 = it;
+	it5 = it4 = s.end();
+	delete &s;
+	std::cout << "1:" << *(void**)&it << ":" << *it << std::endl;
+	--it;
+	std::cout << "2:" << *(void**)&it << ":" << *it << std::endl;
+	--it;
+	std::cout << "3:" << *(void**)&it5 << ":" << &*it5 << std::endl;
+	--it5;
+	std::cout << "4:" << *(void**)&it5 << ":" << &*it5 << std::endl;
+	--it5;
+	std::cout << "5:" << *(void**)&it5 << ":" << &*it5 << std::endl;
+}
